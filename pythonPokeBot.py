98be4a408bcf040e650a01d53b280822b96eb1f2
Tobias Racedo <tobaracedo@gmail.com>
2022-07-19 11:29:50 -0300

    Telegram bot V1

---
+++ pythonPokeBot.py
@@ -0,0 +1,26 @@
+import requests
+
+import telebot # Importamos las librería
+TOKEN = "5355701474:AAFK2b0sdR8T4Qeftc84DmhxBZoQ1t5C1t8" # Ponemos nuestro Token generado con el @BotFather
+bot = telebot.TeleBot(TOKEN) 
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "This bot returns the description of the chosen Pokemon. To choose a Pokemon, enter its name")
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    try:
+        pokemon = requests.get(f'https://pokeapi.co/api/v2/pokemon/{message.text.lower()}')
+        toJson = pokemon.json()
+        secondCall = requests.get(toJson['species']['url'])
+        secondToJson = secondCall.json()
+        data = secondToJson['flavor_text_entries'][0]['flavor_text']
+        newData= ''
+        for l in data:
+            if l == '\n':
+                l = ' '
+            newData = newData + l
+        bot.reply_to(message, newData)
+    except:
+        bot.reply_to(message, 'The selected Pokemon was not found. This can be due to it does not exist or its not currently available on our database.')
+bot.polling()
